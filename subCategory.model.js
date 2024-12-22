732333f01f4e5d56cdd6d105d7491bdf353a2404
Arpita Sahoo <arpita.sahoo2k5@gmail.com>
2024-12-22 16:16:34 +0530

    server

---
+++ subCategory.model.js
@@ -0,0 +1,25 @@
+const subCategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      default: "",
+    },
+    image: {
+      type: String,
+      default: "",
+    },
+    category: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "category",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const SubCategoryModel = mongoose.model("subCategory", subCategorySchema);
+
+export default SubCategoryModel;
